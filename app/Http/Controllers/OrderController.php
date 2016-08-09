cfe0d431b832d4e747b6dd4d52b201b2fc8fc299
Iftikhar uddin <iftipro@gmail.com>
2016-08-09 22:26:03 +0500

    Removed the empty functions

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -23,20 +23,12 @@ class OrderController extends Controller
      */
     public function index()
     {
-		$products = Product::all();
-		$getid = OrderDetail::all();
+	$products = Product::all();
+	$getid = OrderDetail::all();
         return view('orders.index')->with('products', $products)->with('orders', $getid)->with('orderby', $getid);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+   
 
     /**
      * Store a newly created resource in storage.
@@ -72,48 +64,4 @@ class OrderController extends Controller
 		
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 }
